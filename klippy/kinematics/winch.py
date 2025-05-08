04102396ef79373a152c44e5784eacad4292936b
eyll24 <eylul_2002_kahraman@hotmail.com>
2025-05-08 22:24:51 +0300

    Update winch.py

--- klippy/kinematics/winch.py
+++ klippy/kinematics/winch.py
@@ -23,18 +23,20 @@ class WinchKinematics:
             s.setup_itersolve('winch_stepper_alloc', *a)
             s.set_trapq(toolhead.get_trapq())
             toolhead.register_step_generator(s.generate_steps)
+            # Get offset for this specific stepper
+            qx = stepper_config.getfloat("offset_x", default=0.0)
+            qy = stepper_config.getfloat("offset_y", default=0.0)
+            qz = stepper_config.getfloat("offset_z", default=0.0)
+            self.offsets.append((qx, qy, qz))
         # Setup boundary checks
         acoords = list(zip(*self.anchors))
         self.axes_min = toolhead.Coord(*[min(a) for a in acoords], e=0.)
         self.axes_max = toolhead.Coord(*[max(a) for a in acoords], e=0.)
         self.set_position([0., 0., 0.], "")
-        for i in range(len(self.anchors)):
-            qx = stepper_config.getfloat("offset_x", default=0.0)
-            qy = stepper_config.getfloat("offset_y", default=0.0)
-            qz = stepper_config.getfloat("offset_z", default=0.0)
-            self.offsets.append((qx, qy, qz))
+        
     def get_steppers(self):
         return list(self.steppers)
+    
     def calc_position(self, stepper_positions):
         # Get cable lengths (not squared yet)
         pos = [stepper_positions[rail.get_name()] for rail in self.steppers[:3]]
@@ -44,19 +46,24 @@ class WinchKinematics:
             for a, q in zip(self.anchors[:3], self.offsets[:3]) ]
         # Forward kinematics: solve for P such that ||P - (A_i + q_i)|| = L_i
         return mathutil.trilateration(effective_anchors, [p * p for p in pos])
+    
     def set_position(self, newpos, homing_axes):
         for s in self.steppers:
             s.set_position(newpos)
+    
     def clear_homing_state(self, clear_axes):
         # XXX - homing not implemented
         pass
+    
     def home(self, homing_state):
         # XXX - homing not implemented
         homing_state.set_axes([0, 1, 2])
         homing_state.set_homed_position([0., 0., 0.])
+    
     def check_move(self, move):
         # XXX - boundary checks and speed limits not implemented
         pass
+    
     def get_status(self, eventtime):
         # XXX - homed_checks and rail limits not implemented
         return {
